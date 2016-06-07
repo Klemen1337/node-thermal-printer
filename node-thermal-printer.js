b128dc4fdd286dffa50fc4c2763b684e23f9bc1f
Dustin Bolton <dustin@microswift.com>
2016-06-07 17:32:43 -0500

    Fix broken isPrinterConnected() function calling undefined function exists().

--- node-thermal-printer.js
+++ node-thermal-printer.js
@@ -231,7 +231,7 @@ module.exports = {
     if(printerConfig.interface){
       var fs = require('fs');
       fs.exists(printerConfig.interface, function(ex){
-        exists(ex);
+        return ex;
       });
     }
 
