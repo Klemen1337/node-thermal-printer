8eec338593596a7e3ff9f9e636f2a5245811279d
hesos <alberto.teijeiro@gmail.com>
2017-08-30 16:32:17 +0200

    Increasing speed of _printImageBufferEpson
    adding the bytes from an array instead of using concat increases the speed by leaps.

--- node-thermal-printer.js
+++ node-thermal-printer.js
@@ -605,6 +605,7 @@ module.exports = {
       pixels.push(line);
     }
 
+    var imageBuffer_array=[];
     var imageBuffer = new Buffer([]);
     for (var i = 0; i < height; i++) {
       for (var j = 0; j < parseInt(width/8); j++) {
@@ -621,9 +622,11 @@ module.exports = {
           }
         }
 
-        imageBuffer = Buffer.concat([imageBuffer, new Buffer([byte])]);
+        imageBuffer_array.push(byte);
+       // imageBuffer = Buffer.concat([imageBuffer, new Buffer([byte])]);
       }
     }
+    imageBuffer= Buffer.from(imageBuffer_array);
 
     // Print raster bit image
     // GS v 0
