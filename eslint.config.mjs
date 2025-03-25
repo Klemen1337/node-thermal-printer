2b34e7c9b3a184eb68cec72ca0294dc5ca63d7ea
Klemen Kastelic <klemen.kast@gmail.com>
2025-03-25 09:06:34 +0100

    - fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,27 +35,3 @@ export default [
     }
   }
 ];
-
-// "no-var": "warn",
-// "semi": "warn",
-// "console-log": "warn",
-// "no-case-declarations": "off",
-// "global-require": "off",
-// "radix": "off",
-// "no-plusplus": "off",
-// "no-useless-catch": "off",
-// "no-unused-vars": "warn",
-// "no-param-reassign": "off",
-// "space-before-function-paren": "error",
-// "class-methods-use-this": "off",
-// "prefer-promise-reject-errors": "off",
-// "no-throw-literal": "off",
-// "eqeqeq": "off",
-// "no-bitwise": "off",
-// "no-restricted-syntax": "off",
-// "no-control-regex": "off",
-// "guard-for-in": "off",
-// "max-len": [
-//   "error",
-//   140
-// ]
